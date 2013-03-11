52bca50b990a4d56bc1dbf0a0584b98f6c6fb790
Phoenix <georgewatts@georges-macbook-pro.home>
2013-03-11 19:09:22 +0000

    Eventual GUI class file

---
+++ Phoenix's Java Libs/src/com/herobrinesarmy/ChatInterface/ClientGUI.java
@@ -0,0 +1,13 @@
+package com.herobrinesarmy.ChatInterface;
+
+public class ClientGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
