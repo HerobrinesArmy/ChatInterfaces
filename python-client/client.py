48e8948ead7ece5e71f94adbd9e3a32d08bbc5f8
Lucus16 <lars.jellema@gmail.com>
2013-05-06 01:41:14 +0200

    Python client cleanup and improvements.

--- python-client/client.py
+++ python-client/client.py
@@ -7,6 +7,7 @@ import readline
 import getpass
 import random
 import socket
+import queue
 import json
 import time
 import re
@@ -29,19 +30,34 @@ class ChatClient:
         self.htmlentre = re.compile(r'&([^;]+);')
         self.onreceive = []
         self.doautohandle = False
+        self.lastsend = 0
+        self.postqueue = queue.Queue()
+        self.run = False
 
-    def startautohandle(self):
-        self.doautohandle = True
-        self.ahthread = threading.Thread(target=self.autohandler)
-        self.ahthread.start()
+    def postloop(self):
+        while self.run:
+            self.postmessage(self.postqueue.get())
+            time.sleep(2)
 
-    def stopautohandle(self):
-        self.doautohandle = False
+    def start(self):
+        if not self.auth(): return
+        self.run = True
+        self.getthread = threading.Thread(target=self.getloop)
+        self.getthread.start()
+        self.postthread = threading.Thread(target=self.postloop)
+        self.postthread.start()
 
-    def autohandler(self):
-        if not self.isauthed(): return
+    def stop(self):
+        self.run = False
+        self.postthread.join()
+        self.getthread.join()
+
+    def post(self, text):
+        self.postqueue.put(text)
+
+    def getloop(self):
         msgs = self.getmessages()
-        while self.doautohandle:
+        while self.run:
             for msg in msgs:
                 text = self.unescape(msg['message'])
                 user = self.striphtml(msg['user'])
@@ -51,8 +67,17 @@ class ChatClient:
             msgs = self.getmessages()
 
     def setchannel(self, channel):
-        self.chatroom = channel
+        if not self.postqueue.empty():
+            return False
+        if channel == 'main':
+            channel = '8613406'
+        if channel == 'meeting room':
+            channel = '3'
+        if channel == 'wolf':
+            channel = '490'
+        self.channel = channel
         self.lmid = '0'
+        return True
 
     def auth(self, username=None, password=None):
         if username: self.username = username
@@ -68,17 +93,18 @@ class ChatClient:
             data = bytes('user=' + self.username + '&pass=' + self.password,
                          'utf-8')
             self.opener.open(self.domain + '/auth.php', data, timeout=60)
-        if self.isauthed():
-            return True
-        return False
+        return self.isauthed()
 
     def isauthed(self):
-        return self.opener.open(self.domain + '/amiauth').read() != b'Nope.'
+        return self.opener.open(self.domain + '/amiauth').read() == b'Yeah.'
 
     def getmessages(self):
         url = self.domain + '/update_chat2.php?c=' + self.chatroom
         url += '&l=' + self.lmid + '&p=0'
-        response = self.opener.open(url, timeout=60).read().decode('utf-8')
+        try:
+            response = self.opener.open(url, timeout=60).read().decode('utf-8')
+        except:
+            pass
         try:
             parsed = json.loads(response[1:-1])
         except ValueError:
@@ -86,17 +112,20 @@ class ChatClient:
         self.lmid = parsed['lmid']
         self.users = parsed['users']
         try:
-            return list(parsed['messages'].values())
+            msgs = list(parsed['messages'].items())
+            msgs.sort()
+            return [b for a, b in msgs]
         except KeyError:
             return []
 
     def postmessage(self, text):
         encoded = urllib.parse.quote(text.strip()[:512])
         if encoded == '': return False
-        if not self.auth():
-            return False
+        if time.time() - self.lastsend < 2:
+            time.sleep(2)
         self.opener.open(self.domain + '/post_chat.php?c=' + self.chatroom +
                          '&o=1&m=' + encoded, timeout=60)
+        self.lastsend = time.time()
 
     def striphtml(self, text):
         return self.htmlre.sub('', text)
@@ -109,35 +138,59 @@ class ChatClient:
 if __name__ == '__main__':
     def printmid(text):
         buffer = readline.get_line_buffer()
-        print('\r' + text + ' ' * min(len(buffer) - len(text) + 4, 79 - len(text))
-              + '\n  > ' + buffer, end='')
+        print('\r' + text + ' ' * min(len(buffer) - len(text) + 4, 79 -
+                                      len(text)) + '\n  > ' + buffer, end='')
         readline.redisplay()
+    def printchat(text, user):
+        if text.startswith('/me'):
+            text = '* ' + user + text[3:]
+        else:
+            text = user + ': ' + text
+        printmid(text)
 
     with open('../HA-Chat-bash/wolf.txt', 'r') as f:
         wolves = [x.strip() for x in f.readlines()]
-    random.shuffle(wolves)
-    nextwolf = 0
-    wolfnum = len(wolves)
+    lastwolf = 0
+    wolfcount = len(wolves)
     try:
         client = ChatClient()
-        client.auth()
-        client.onreceive.append(lambda text, user: printmid(user + ': ' + text))
-        client.startautohandle()
+        if not client.auth():
+            print('Could not authorize.')
+            exit()
+        client.onreceive.append(printchat)
+        client.start()
         cmd = ''
         while cmd not in ['/exit', '/quit']:
             if cmd == '':
                 pass
             elif cmd.startswith('/wolf'):
-                client.postmessage('[img]' + wolves[nextwolf] + '[/img]')
-                nextwolf = (nextwolf + 1) % wolfnum
+                c = cmd[6:].strip()
+                if c.startswith('count'):
+                    printmid('Number of wolves loaded: ' + str(wolfcount))
+                elif c.startswith('previous'):
+                    printmid('Last posted wolf was: ' + str(lastwolf + 1))
+                else:
+                    if ' ' in c: c = c[:c.find(' ')]
+                    try:
+                        lastwolf = int(c) - 1
+                        if lastwolf < 0 or lastwolf >= wolfcount:
+                            raise ValueError
+                    except ValueError:
+                        lastwolf = random.randint(0, wolfcount - 1)
+                    client.post('[img]' + wolves[lastwolf] + '[/img]')
             elif cmd.startswith('/eval '):
-                print(str(eval(cmd[5:]))[:512])
+                print(str(eval(cmd[6:]))[:512])
+            elif cmd.startswith('/robo '):
+                client.post(' '.join('-'.join(list(x))
+                                     for x in cmd[6:].split(' ')))
+            elif cmd.startswith('/room '):
+                if not client.setchannel(cmd[6:]):
+                    printmid('WARNING: Could not switch channels because ' +
+                             'the post queue is not empty.')
             else:
-                client.postmessage(cmd)
+                client.post(cmd)
             cmd = input('  > ')
     except Exception as e:
-        client.stopautohandle()
-        client.ahthread.join()
+        client.stop()
         raise e
-    client.stopautohandle()
-    client.ahthread.join()
+    client.stop()
