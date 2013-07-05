a73a7c867b7b2e761e2589cda0109ac22389a7f3
Phoenix <georgewatts@macbook pro.home>
2013-07-05 02:05:49 +0100

    Updated authors

--- JavaClient/src/org/herobrinesarmy/javaclient/entities/Message.java
+++ JavaClient/src/org/herobrinesarmy/javaclient/entities/Message.java
@@ -3,12 +3,14 @@ package org.herobrinesarmy.javaclient.entities;
 /**
  * A class for a message on herobrinesarmy.com, handles any bbcode tags, emoticons, and links within the message.
  *
- * @author Phoenix, LukeW4lker
+ * @author Phoenix
+ * @author LukeW4lker
  * @version 0.1
  */
 public class Message {
 
     public Message() {
+
     }
 
 }
