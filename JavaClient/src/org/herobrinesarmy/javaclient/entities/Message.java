571a8fb66c4115230439f68709bb09bfaacc9a05
Phoenix <georgewatts@macbook pro.home>
2013-07-05 01:44:19 +0100

    Initial Commit
    Created base Message class.

---
+++ JavaClient/src/org/herobrinesarmy/javaclient/entities/Message.java
@@ -0,0 +1,14 @@
+package org.herobrinesarmy.javaclient.entities;
+
+/**
+ * A class for a message on herobrinesarmy.com, handles any bbcode tags, emoticons, and links within the message.
+ *
+ * @author Phoenix, LukeW4lker
+ * @version 0.1
+ */
+public class Message {
+
+    public Message() {
+    }
+
+}
