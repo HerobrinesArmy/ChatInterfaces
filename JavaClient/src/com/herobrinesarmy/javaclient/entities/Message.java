cef2358512fcd802971c88e4ee7fbe5484bcd820
Phoenix <georgewatts@macbook pro.home>
2013-07-06 18:46:09 +0100

    Added basic code for a message in a chatroom

--- JavaClient/src/com/herobrinesarmy/javaclient/entities/Message.java
+++ JavaClient/src/com/herobrinesarmy/javaclient/entities/Message.java
@@ -1,5 +1,7 @@
 package com.herobrinesarmy.javaclient.entities;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 /**
  * A class for a message on herobrinesarmy.com, handles any bbcode tags, emoticons, and links within the message.
  *
@@ -9,8 +11,41 @@ package com.herobrinesarmy.javaclient.entities;
  */
 public class Message {
 
-    public Message() {
+    private int id;
+    private String timestamp;
+    private String user;
+    private String messageText;
 
+    protected Message(int id, String time, String user, String text) {
+        this.id = id;
+        this.timestamp = time;
+        this.user = user;
+        this.messageText = text;
     }
 
+    public int getMessageID() {
+        return id;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public String toString() {
+        if(messageText.startsWith("/me")) {
+            messageText = messageText.substring(messageText.indexOf("/me") + 3);
+            return StringEscapeUtils.unescapeHtml4("[" + timestamp + "] " + user + messageText);
+        }
+        else {
+            return StringEscapeUtils.unescapeHtml4("[" + timestamp + "] " + user + ": " + messageText);
+        }
+    }
 }
